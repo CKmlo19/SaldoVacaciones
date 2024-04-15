ebe3f73a11f74887de3b71461ac8f55e2ca620fa
Camilo Orocú <luiscamilooc@gmail.com>
2024-04-15 16:26:11 -0600

    Creando un modal

--- SaldoVacaciones/obj/Debug/net8.0/SaldoVacaciones.AssemblyInfo.cs
+++ SaldoVacaciones/obj/Debug/net8.0/SaldoVacaciones.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaldoVacaciones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+173ec6b8cfa328c9c1e4af5cde7816a19ac983a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efef54415f3432be934c6f234b2054f9d2606017")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaldoVacaciones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaldoVacaciones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
